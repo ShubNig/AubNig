d47bbeb9ca0e7673561d0466c1e9a03da4ae0322
sinlov <sinlovgmppt@gmail.com>
2018-02-18 23:48:04 +0800

    refactor config.go

--- aubnig/config.go
+++ aubnig/config.go
@@ -2,11 +2,11 @@ package aubnig
 
 import (
 	"path/filepath"
-	"github.com/sinlov/golang_utils/files"
-	sCli "github.com/sinlov/golang_utils/cli"
 	"fmt"
 	"os"
 	"strings"
+	sFiles "github.com/sinlov/golang_utils/files"
+	sCli "github.com/sinlov/golang_utils/cli"
 )
 
 // if not find config Path just try to use GOPATH code github.com/ShubNig/AubNig/config.conf
@@ -14,24 +14,24 @@ import (
 func Try2FindOutConfigPath() (string, string) {
 	configFilePath := filepath.Join(sCli.CommandPath(), "aubnig.conf")
 	projectPath := sCli.CurrentDirectory()
-	if files.IsFileExist(configFilePath) {
+	if sFiles.IsFileExist(configFilePath) {
 		return configFilePath, projectPath
 	}
 	fmt.Printf("\nWarning!\ncan not find config.conf file at aubnig path: %s\n", sCli.CommandPath())
-	go_path_env := os.Getenv("GOPATH")
-	go_path_env_s := strings.Split(go_path_env, ":")
-	is_find_dev_conf := false
-	for _, path := range go_path_env_s {
+	goPathEnv := os.Getenv("GOPATH")
+	goPathEnvS := strings.Split(goPathEnv, ":")
+	isFindDevConf := false
+	for _, path := range goPathEnvS {
 		codePath := filepath.Join(path, "src", "github.com", "ShubNig", "AubNig")
 		futurePath := filepath.Join(codePath, "aubnig.conf")
 		projectPath = filepath.Join(codePath, "build")
-		if files.IsFileExist(futurePath) {
+		if sFiles.IsFileExist(futurePath) {
 			configFilePath = futurePath
-			is_find_dev_conf = true
+			isFindDevConf = true
 			break
 		}
 	}
-	if is_find_dev_conf {
+	if isFindDevConf {
 		fmt.Printf("just use dev config at path: %s\n", configFilePath)
 	} else {
 		fmt.Printf("can not load config at path: %s\nExit 1\n", configFilePath)
